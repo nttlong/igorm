35380b68631db2f8ee1580cc413216cf03100006
Nttlong <zugeliang2000@gmail.com>
2025-06-22 18:19:51 +0700

    fix

--- unvs-core/go.mod
+++ unvs-core/go.mod
@@ -2,4 +2,21 @@ module unvs-core
 
 go 1.24.3
 
+require dbx v0.0.0-20241012123456-abcdef123456 // pseudo-version for local development
+
+require (
+	filippo.io/edwards25519 v1.1.0 // indirect
+	github.com/go-sql-driver/mysql v1.9.2 // indirect
+	github.com/golang-sql/civil v0.0.0-20220223132316-b832511892a9 // indirect
+	github.com/golang-sql/sqlexp v0.1.0 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/lib/pq v1.10.9 // indirect
+	github.com/microsoft/go-mssqldb v1.8.1 // indirect
+	github.com/xwb1989/sqlparser v0.0.0-20180606152119-120387863bf2 // indirect
+	golang.org/x/crypto v0.24.0 // indirect
+	golang.org/x/text v0.24.0 // indirect
+	google.golang.org/genproto v0.0.0-20250512202823-5a2f75b736a9 // indirect
+	google.golang.org/protobuf v1.36.6 // indirect
+)
+
 replace dbx => ../dbx
---
+++ unvs-core/models/roles.go
@@ -0,0 +1,22 @@
+package models
+
+import (
+	"dbx"
+	"time"
+)
+
+type Role struct {
+	dbx.EntityModel `db:"Role"`
+	Id              int        `db:"pk;auto" json:"id"`
+	RoleId          string     `db:"varchar(36);uk" json:"roleId"`
+	Code            string     `db:"uk;varchar(50)" json:"code"`
+	Name            string     `db:"uk;varchar(50)" json:"name"`
+	CreatedAt       time.Time  `json:"createdAt"`
+	UpdatedAt       *time.Time `json:"updatedAt,omitempty"`
+	CreatedBy       string     `db:"varchar(50);idx" json:"createdBy,omitempty"`
+	UpdatedBy       *string    `db:"varchar(50);idx" json:"updatedBy,omitempty"`
+
+	Description dbx.FullTextSearchColumn `json:"description" swag:"-"`
+	Users       []User                   `db:"fk:RoleId" json:"users,omitempty"`
+	ViewRoles   []ViewRole               `db:"fk:RoleId" json:"viewRoles,omitempty"`
+}
