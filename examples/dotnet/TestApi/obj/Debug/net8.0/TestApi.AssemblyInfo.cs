c318c17c1e09f28ea8be8ae3e3006fd39160f714
Nttlong <zugeliang2000@gmail.com>
2025-08-21 22:21:11 +0700

    Support auth context

--- examples/dotnet/TestApi/obj/Debug/net8.0/TestApi.AssemblyInfo.cs
+++ examples/dotnet/TestApi/obj/Debug/net8.0/TestApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b9fa435ac8aa294b6f59974a330921db894236d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8731d38cb051f5dbc1dd6162caaeb6652461649")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
