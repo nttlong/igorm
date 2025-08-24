65842f10383d7e9a0a88fdc41539d778ed83c370
Nttlong <zugeliang2000@gmail.com>
2025-08-24 21:25:12 +0700

    bm test

--- packages/xauth/services/UserService_test.go
+++ packages/xauth/services/UserService_test.go
@@ -2,6 +2,7 @@ package services
 
 import (
 	"testing"
+	"vdb"
 	"wx"
 	dbmodels "xauth/dbModels"
 
@@ -21,6 +22,39 @@ func TestUserserviceCreateUser(t *testing.T) {
 		Username: "test001",
 	}
 	err = ret.CreateUser(tenantDb, user, "test001")
-	assert.NoError(t, err)
+	if err != nil {
+		var vErr *vdb.DialectError
+		if !assert.ErrorAs(t, err, &vErr) {
+			t.FailNow()
+		}
+		assert.Equal(t, vdb.DIALECT_DB_ERROR_TYPE_DUPLICATE, vErr.ErrorType)
 
+	}
+
+}
+func BenchmarkUserserviceCreateUser(b *testing.B) {
+	db, err := wx.NewGlobal[DbService]()
+	assert.NoError(b, err)
+	assert.NotNil(b, db)
+	assert.NoError(b, err)
+	tenantDb, err := db.GetTenantDb("test001")
+	assert.NoError(b, err)
+	ret, err := wx.NewDepend[UserService]()
+	assert.NoError(b, err)
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+
+		user := &dbmodels.Users{
+			Username: "test001",
+		}
+		err = ret.CreateUser(tenantDb, user, "test001")
+		if err != nil {
+			var vErr *vdb.DialectError
+			if !assert.ErrorAs(b, err, &vErr) {
+				b.FailNow()
+			}
+			assert.Equal(b, vdb.DIALECT_DB_ERROR_TYPE_DUPLICATE, vErr.ErrorType)
+
+		}
+	}
 }
